3aac386b16365641d02e404857f4050cc92dad85
arnav <arnavj3@illinois.edu>
2018-06-22 15:40:59 +0530

    Raspberry Pi code

---
+++ ultrasonic_distance.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+import urllib2
+import json
+
+url = 'https://temp-data-cf6e2.firebaseio.com/tempdata.json'
+
+
+GPIO.setmode(GPIO.BCM)
+
+trigger = 18
+echo = 24
+
+GPIO.setup(trigger, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+
+def distance():
+	#set trigger to high
+	GPIO.output(trigger, True)
+	
+	#set trigger after 0.01ms to low
+	time.sleep(0.00001)
+	GPIO.output(trigger, False)
+
+	StartTime = time.time()
+	StopTime = time.time()
+
+	#save StartTime
+	while GPIO.input(echo) == 0:
+		StartTime = time.time()
+
+	#save time of arrival 
+	while GPIO.input(echo) == 1:
+		StopTime = time.time()
+
+
+	#time difference between start and arrival
+	TimeElapsed = StartTime - StopTime
+
+	distance = (TimeElapsed * 34300)/2
+
+	return abs(distance) 
+
+def sendData():
+	postdata = {
+		'Distance': distance()
+	}
+
+	req = urllib2.Request(url)
+	req.add_header('Content-Type','application/json')
+	data = json.dumps(postdata)
+
+	response = urllib2.urlopen(req,data)
+
+
+if __name__ == '__main__':
+	try:
+		while True:
+			dist = distance()
+			print("Measured distance = %.1f cm" %dist)
+			sendData()
+			#time.sleep(1)
+
+	except KeyboardInterrupt:
+		print "-- Measurement Stopped --"
+		GPIO.cleanup()
+
+
+
